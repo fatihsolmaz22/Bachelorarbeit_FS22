37a407360da24114bd23d32996b6f8225fb04f02
Manu Kunnumpurathu <kunnuman@students.zhaw.ch>
2022-03-08 19:13:48 +0100

    added a cli_executor

---
+++ 02_Code/ba_code/cli_executor.py
@@ -0,0 +1,28 @@
+import typer
+from ba_code.tripadvisor_extraction import extractor
+
+app = typer.Typer()
+
+
+@app.command()
+def extract_reviews():
+    """
+            \tThis command extracts online reviews from Tripadvisor
+    """
+    extractor.main()
+
+
+@app.command()
+def say_hello(name: str, iq: int, display_iq: bool = True):
+    """
+        Say hi to NAME, optionally with a --lastname.
+
+        If --formal is used, say hi very formally.
+    """
+    typer.echo(f"Hello {name}")
+    if display_iq:
+        typer.echo(f"Your iq is {iq}")
+
+
+if __name__ == "__main__":
+    app()
