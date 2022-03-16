429ee56df2f263382334b89df1d0bee10e28e0c8
Manu Kunnumpurathu <kunnuman@students.zhaw.ch>
2022-03-16 01:44:38 +0100

    fixed some small bugs and implemented review and turnover plot

--- 02_Code/ba_code/ba_code/tripadvisor_extraction/review_data_analyzer.py
+++ 02_Code/ba_code/ba_code/tripadvisor_extraction/review_data_analyzer.py
@@ -5,6 +5,7 @@ from ba_code.tripadvisor_extraction.tripadvisor_strings import RestaurantURLs
 from ba_code.restaurant_data_preprocessing.restaurant_data_extractor import RestaurantDataExtractor
 from ba_code.restaurant_data_preprocessing.restaurant_uri import RestaurantUri
 
+
 class ReviewDataAnalyzer:
 
     # def __init__(self):
@@ -16,24 +17,39 @@ class ReviewDataAnalyzer:
         path_to_restaurant_json = template.format(restaurant_enum.name)
         df = pd.read_json(path_to_restaurant_json)
         df = df.filter(items=["date", "rating"])
-        return df.groupby(["date"]).mean()
+        return df.groupby(pd.Grouper(key='date', axis=0,
+                                     freq='m')).mean().fillna(0)
+
 
 def main():
     review_data_analyzer = ReviewDataAnalyzer()
-    df_review_data = review_data_analyzer.get_monthly_rating_dataframe_for_restaurant(RestaurantURLs.NOOCH_BADENERSTRASSE)
+    df_review_data = review_data_analyzer.get_monthly_rating_dataframe_for_restaurant(
+        RestaurantURLs.NOOCH_BADENERSTRASSE)
 
     restaurant_data_extractor = RestaurantDataExtractor()
     df_restaurant_data = restaurant_data_extractor.get_turnover_per_month_dataframe(RestaurantUri.NOOCH_BADENERSTRASSE)
 
     print(df_review_data.head(5))
+    print(df_restaurant_data.head(5))
 
-    plt.figure(1)
-    df_review_data.plot()
-    plt.show()
+    # plot ###################################################################
+    color = 'red'
+    fig, ax1 = plt.subplots()
+    ax1.set_xlabel('date ')
+    ax1.set_ylabel('rating', color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+    df_review_data.plot(ax=ax1, color=color)
 
-    plt.figure(2)
-    df_restaurant_data.plot()
+    color = 'blue'
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    ax2.set_ylabel('turnover in CHF', color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+    df_restaurant_data.plot(ax=ax2, color=color)
+
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    plt.legend(loc="upper left")
     plt.show()
+    # plot ###################################################################
 
 if __name__ == '__main__':
     main()
