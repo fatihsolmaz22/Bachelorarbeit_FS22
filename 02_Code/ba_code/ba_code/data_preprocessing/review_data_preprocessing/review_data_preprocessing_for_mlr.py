8bf2d745bf51a95e18e0979df6aa3b84cdc0cec4
Manu Kunnumpurathu <kunnuman@students.zhaw.ch>
2022-03-29 21:50:52 +0200

    small refactoring

--- 02_Code/ba_code/ba_code/data_preprocessing/review_data_preprocessing/review_data_preprocessing_for_mlr.py
+++ 02_Code/ba_code/ba_code/data_preprocessing/review_data_preprocessing/review_data_preprocessing_for_mlr.py
@@ -4,10 +4,69 @@ import numpy as np
 from datetime import datetime
 from ba_code.data_preprocessing.review_data_preprocessing.review_uri import ReviewUri
 
-"""Lieber Manu Grundsätzlich kann man das schon ausmultiplizieren. Du musst einfach das Datum in einen numerischen 
-Wert umwandeln. Z.B. Tage seit heute. Besser als Tage seit heute wäre wohl noch der Kehrwert, 1/Tage. Zur Normierung 
-würde ich den ganzen Ausdruck in erster Näherung noch durch die Anzahl Reviews teilen. Hilft das? Ist das überhaupt 
-das was Du meinst? Liebe Grüsse Martin """
+
+def get_mlr_equations_dataframe():
+    overall_ratings = []
+    expression_0 = []
+    expression_1 = []
+    expression_2 = []
+    expression_3 = []
+    expression_4 = []
+    expression_5 = []
+    expression_6 = []
+    expression_7 = []
+    expression_8 = []
+
+    # read review data for each restaurant
+    for review_uri in ReviewUri:
+        review_data_of_restaurant = json.load(open(review_uri.value))
+
+        # extracting important variables for the equation
+        number_of_reviews = len(review_data_of_restaurant['all_reviews'])
+        overall_rating = review_data_of_restaurant['overall_rating']
+        all_reviews = review_data_of_restaurant['all_reviews']
+
+        # extracting important properties from author_data and review_data
+        [author_data, review_data] = extract_author_and_review_data(all_reviews)
+        [author_levels, contributions, helpful_votes, author_distributions] = \
+            extract_important_properties_from_author_data(author_data)
+        author_distribution_levels = extract_author_distribution_levels(author_distributions)
+        [age_of_reviews_in_days, ratings, likes] = extract_important_properties_from_review_data(review_data)
+
+        # build equations, TODO special reviews: include: https://www.tripadvisor.ch/Restaurant_Review-g60763-d1236281-Reviews-or70-Club_A_Steakhouse-New_York_City_New_York.html
+        overall_ratings.append(overall_rating)
+        expression_0.append(
+            (1 / number_of_reviews) * np.sum(np.multiply(ratings, np.reciprocal(age_of_reviews_in_days))))
+        expression_1.append((1 / number_of_reviews) * np.sum(np.multiply(ratings, likes)))
+        expression_2.append((1 / number_of_reviews) * np.sum(np.multiply(ratings, author_levels)))
+        expression_3.append((1 / number_of_reviews) * np.sum(np.multiply(ratings, helpful_votes)))
+        # TODO: fix contributions bug, some values are zero, and check if expressions are calculated correctly
+        reciprocal_of_contributions = np.reciprocal(contributions)
+        expression_4.append((1 / number_of_reviews) *
+                            np.sum(reciprocal_of_contributions * ratings * author_distribution_levels[0]))
+        expression_5.append((1 / number_of_reviews) *
+                            np.sum(reciprocal_of_contributions * ratings * author_distribution_levels[1]))
+        expression_6.append((1 / number_of_reviews) *
+                            np.sum(reciprocal_of_contributions * ratings * author_distribution_levels[2]))
+        expression_7.append((1 / number_of_reviews) *
+                            np.sum(reciprocal_of_contributions * ratings * author_distribution_levels[3]))
+        expression_8.append((1 / number_of_reviews) *
+                            np.sum(reciprocal_of_contributions * ratings * author_distribution_levels[4]))
+
+    df_mlr_equations = pd.DataFrame({
+        "overall_rating": overall_ratings,
+        "expression_0": expression_0,
+        "expression_1": expression_1,
+        "expression_2": expression_2,
+        "expression_3": expression_3,
+        "expression_4": expression_4,
+        "expression_5": expression_5,
+        "expression_6": expression_6,
+        "expression_7": expression_7,
+        "expression_8": expression_8,
+    })
+
+    return df_mlr_equations
 
 
 def extract_author_and_review_data(all_reviews):
@@ -68,58 +127,4 @@ def extract_important_properties_from_review_data(review_data):
 
 
 ########################################################################################################################
-overall_ratings = []
-expression_0 = []
-expression_1 = []
-expression_2 = []
-expression_3 = []
-expression_4 = []
-expression_5 = []
-expression_6 = []
-expression_7 = []
-expression_8 = []
-
-# read review data for each restaurant
-for review_uri in ReviewUri:
-    review_data_of_restaurant = json.load(open(review_uri.value))
-
-    # extracting important variables for the equation
-    number_of_reviews = len(review_data_of_restaurant['all_reviews'])
-    overall_rating = review_data_of_restaurant['overall_rating']
-    all_reviews = review_data_of_restaurant['all_reviews']
-
-    # extracting important properties from author_data and review_data
-    [author_data, review_data] = extract_author_and_review_data(all_reviews)
-    [author_levels, contributions, helpful_votes, author_distributions] = extract_important_properties_from_author_data(
-        author_data)
-    author_distribution_levels = extract_author_distribution_levels(author_distributions)
-    [age_of_reviews_in_days, ratings, likes] = extract_important_properties_from_review_data(review_data)
-
-    # build equations, TODO special reviews: include: https://www.tripadvisor.ch/Restaurant_Review-g60763-d1236281-Reviews-or70-Club_A_Steakhouse-New_York_City_New_York.html
-    overall_ratings.append(overall_rating)
-    expression_0.append((1 / number_of_reviews) * np.sum(np.multiply(ratings, np.reciprocal(age_of_reviews_in_days))))
-    expression_1.append((1 / number_of_reviews) * np.sum(np.multiply(ratings, likes)))
-    expression_2.append((1 / number_of_reviews) * np.sum(np.multiply(ratings, author_levels)))
-    expression_3.append((1 / number_of_reviews) * np.sum(np.multiply(ratings, helpful_votes)))
-    # TODO: fix contributions bug, some values are zero, and check if expressions are calculated correctly
-    reciprocal_of_contributions = np.reciprocal(contributions)
-    expression_4.append((1 / number_of_reviews) * np.sum(reciprocal_of_contributions * ratings * author_distribution_levels[0]))
-    expression_5.append((1 / number_of_reviews) * np.sum(reciprocal_of_contributions * ratings * author_distribution_levels[1]))
-    expression_6.append((1 / number_of_reviews) * np.sum(reciprocal_of_contributions * ratings * author_distribution_levels[2]))
-    expression_7.append((1 / number_of_reviews) * np.sum(reciprocal_of_contributions * ratings * author_distribution_levels[3]))
-    expression_8.append((1 / number_of_reviews) * np.sum(reciprocal_of_contributions * ratings * author_distribution_levels[4]))
-
-df_mlr_equations = pd.DataFrame({
-        "overall_rating": overall_ratings,
-        "expression_0": expression_0,
-        "expression_1": expression_1,
-        "expression_2": expression_2,
-        "expression_3": expression_3,
-        "expression_4": expression_4,
-        "expression_5": expression_5,
-        "expression_6": expression_6,
-        "expression_7": expression_7,
-        "expression_8": expression_8,
-    }
-)
-
+df = get_mlr_equations_dataframe()
