dc2f1a2a1bb3d1ed481555666c3047f05bfd9b65
Manu Kunnumpurathu <kunnuman@students.zhaw.ch>
2022-04-11 22:32:27 +0200

    implemented incremental_overall_rating and two test files

--- 02_Code/ba_code/ba_code/data_preprocessing/tripadvisor_restaurant_data_preprocessing/tripadvisor_restaurant_data_extractor.py
+++ 02_Code/ba_code/ba_code/data_preprocessing/tripadvisor_restaurant_data_preprocessing/tripadvisor_restaurant_data_extractor.py
@@ -1,8 +1,6 @@
 import json
 import pandas as pd
 import numpy as np
-from ba_code.data_preprocessing.tripadvisor_restaurant_data_preprocessing.tripadvisor_restaurant_data_uri import \
-    TripadvisorRestaurantDataUri
 
 
 class TripadvisorRestaurantDataExtractor:
@@ -121,36 +119,12 @@ class TripadvisorRestaurantDataExtractor:
     def get_author_distribution_dataframe(self):
         return self.__tripadvisor_restaurant_data['author_data']['df_author_distribution']
 
-"""
-tripadvisor_restaurant_data_extractor = TripadvisorRestaurantDataExtractor()
-tripadvisor_restaurant_data_extractor.load_restaurant_data(
-    open(TripadvisorRestaurantDataUri.DIFFERENTE_HOTEL_KRONE.value))
+    def get_incremental_overall_rating_dataframe(self):
+        df_review_data = self.get_review_data_dataframe()
+        df_incremental_overall_rating = pd.DataFrame({
+            'date': df_review_data['date'].to_list(),
+            'incremental_overall_rating': df_review_data['rating'].expanding().mean().to_list()
+        })
+        df_incremental_overall_rating.index.name = df_review_data.index.name
 
-
-df_review_data = tripadvisor_restaurant_data_extractor.get_review_data_dataframe()
-#df_review_data.iloc[1000:1013]
-#df_review_data.iloc[1004]
-#df_review_data.iloc[1012]
-
-contents = df_review_data.content.to_list()
-
-contains_more = []
-for content in contents:
-    contains_more.append("...More" in content)
-
-contains_more = any(contains_more)
-
-
-
-df_review_data = tripadvisor_restaurant_data_extractor.get_review_data_dataframe()
-df_author_base_infos = tripadvisor_restaurant_data_extractor.get_author_base_infos_dataframe()
-df_author_stats = tripadvisor_restaurant_data_extractor.get_author_stats_dataframe()
-df_author_distribution = tripadvisor_restaurant_data_extractor.get_author_distribution_dataframe()
-any_duplicates = any(df_review_data.duplicated().to_list())
-
-print("Restaurant name:", tripadvisor_restaurant_data_extractor.get_restaurant_name())
-print("Any duplicate in reviews:", any_duplicates)
-print("Number of entries in df_review_data", len(df_review_data.index))
-print("Number of entries in df_author_base_infos", len(df_author_base_infos))
-print("Number of entries in df_author_stats", len(df_author_stats))
-print("Number of entries in df_author_distribution", len(df_author_distribution))"""
+        return df_incremental_overall_rating
