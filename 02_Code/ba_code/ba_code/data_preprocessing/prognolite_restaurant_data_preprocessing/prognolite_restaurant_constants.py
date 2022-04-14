3aa7acab47550013953b6cc27a09d1e17da35906
Manu Kunnumpurathu <kunnuman@students.zhaw.ch>
2022-04-15 00:48:22 +0200

    added comment and did some renaming

--- 02_Code/ba_code/ba_code/data_preprocessing/prognolite_restaurant_data_preprocessing/prognolite_restaurant_constants.py
+++ 02_Code/ba_code/ba_code/data_preprocessing/prognolite_restaurant_data_preprocessing/prognolite_restaurant_constants.py
@@ -2,13 +2,14 @@ from enum import Enum
 import os
 from ba_code.path import PROGNOLITE_RESTAURANT_DATA_PATH
 
-path = PROGNOLITE_RESTAURANT_DATA_PATH
-
 
 class PrognoliteRestaurantDataUri(Enum):
-    ALL_RESTAURANT_DATA = os.path.join(path, 'fwg_composition_data_IDP.csv')
+    PROGNOLITE_RESTAURANT_DATA = os.path.join(PROGNOLITE_RESTAURANT_DATA_PATH, 'fwg_composition_data_IDP.csv')
 
-# Restaurants with revenue data are listed here
+
+# Restaurants with revenue data are listed below. Not listed are some restaurants where no revenue data exists.
+# I got these informations by calling PrognoliteRestaurantDataExtractor().get_restaurant_data() which lists
+# all revenue data for all restaurants.
 class Restaurant(Enum):
     BUTCHER_USTER = "fwg-butcher-uster"
     NEGISHI_METALLI = "fwg-negishi-metalli"
@@ -25,4 +26,3 @@ class Restaurant(Enum):
     NEGISHI_STEINEN = "fwg-negishi-steinen"
     NOOCH_USTER = "fwg-nooch-uster"
     BUTCHER_ZUERICH_WEST = "fwg-butcher-zuerichWest"
-
