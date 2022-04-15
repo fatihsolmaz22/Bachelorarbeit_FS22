83bd7be25a09f5b9e6d21bff3603616f68814ad3
Manu Kunnumpurathu <kunnuman@students.zhaw.ch>
2022-04-15 03:33:48 +0200

    implemented get_turnover_development_since_beginning and get_turnover_per_time_period

--- 02_Code/ba_code/ba_code/data_preprocessing/prognolite_restaurant_data_preprocessing/prognolite_restaurant_data_extractor.py
+++ 02_Code/ba_code/ba_code/data_preprocessing/prognolite_restaurant_data_preprocessing/prognolite_restaurant_data_extractor.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from ba_code.data_preprocessing.prognolite_restaurant_data_preprocessing.prognolite_restaurant_constants import PrognoliteRestaurantDataUri
+from ba_code.data_preprocessing.prognolite_restaurant_data_preprocessing.prognolite_restaurant_constants \
+    import PrognoliteRestaurantDataUri, Restaurant
 
 
 class PrognoliteRestaurantDataExtractor:
@@ -9,7 +10,7 @@ class PrognoliteRestaurantDataExtractor:
         self.__initialize_dataframes()
 
     def __initialize_dataframes(self):
-        df = pd.read_csv(PrognoliteRestaurantDataUri.ALL_RESTAURANT_DATA.value).drop_duplicates()
+        df = pd.read_csv(PrognoliteRestaurantDataUri.PROGNOLITE_RESTAURANT_DATA.value).drop_duplicates()
         df['d'] = pd.to_datetime(df['d'])
         restaurant_names = df.drop_duplicates(subset=['tenant'])['tenant'].to_list()
         restaurant_names = [restaurant_name.strip() for restaurant_name in restaurant_names]
@@ -23,23 +24,56 @@ class PrognoliteRestaurantDataExtractor:
     def get_restaurant_names(self):
         return self.__restaurant_data.keys()
 
-    def get_dataframe(self, restaurant):
+    def get_restaurant_data_dataframe(self, restaurant):
         return self.__restaurant_data[restaurant.value]
 
-    def get_turnover_per_day_dataframe(self, restaurant):
+    def get_turnover_development_since_beginning_dataframe(self, restaurant, time_period='m'):
         df_date_turnover = self.__get_date_turnover_dataframe(restaurant)
-        return df_date_turnover.groupby(pd.Grouper(key='d', axis=0,
-                                                   freq='d')).sum()
 
-    def get_turnover_per_month_dataframe(self, restaurant):
+        df_turnover_development_since_beginning = None
+        if time_period == 'd' or time_period == 'm' or time_period == 'Y':
+            df_turnover_development_since_beginning = \
+                df_date_turnover.groupby(pd.Grouper(key='d', axis=0, freq=time_period)) \
+                    .sum()['turnover'].cumsum().reset_index()
+        elif time_period == 'Q':
+            df_turnover_development_since_beginning = \
+                df_date_turnover.groupby(df_date_turnover['d'].dt.to_period(time_period))['turnover'].agg(
+                    'sum').cumsum().reset_index()
+        else:
+            self.__print_invalid_time_period_message()
+            return
+
+        return df_turnover_development_since_beginning
+
+    def get_turnover_per_time_period(self, restaurant, time_period='m'):
         df_date_turnover = self.__get_date_turnover_dataframe(restaurant)
-        return df_date_turnover.groupby(pd.Grouper(key='d', axis=0,
-                                                   freq='m')).sum()
+
+        df_date_turnover_per_time_period = None
+        if time_period == 'd' or time_period == 'm' or time_period == 'Y':
+            df_date_turnover_per_time_period = \
+                df_date_turnover.groupby(pd.Grouper(key='d', axis=0, freq=time_period)).sum().reset_index()
+        elif time_period == 'Q':
+            df_date_turnover_per_time_period = \
+                df_date_turnover.groupby(df_date_turnover['d'].dt.to_period(time_period))['turnover'].agg(
+                    'sum').reset_index()
+        else:
+            self.__print_invalid_time_period_message()
+            return
+
+        return df_date_turnover_per_time_period
+
+    def __print_invalid_time_period_message(self):
+        print("Invalid time period, enter one of the following time periods:")
+        print("'d': Day")
+        print("'m': Month")
+        print("'Q': Quarter")
+        print("'Y': Year")
 
     def __get_date_turnover_dataframe(self, restaurant):
-        df = self.__restaurant_data[restaurant.value]
-        return df.filter(items=['d', 'turnover'])
+        df_restaurant_data = self.__restaurant_data[restaurant.value]
+        df_date_turnover = df_restaurant_data.filter(items=['d', 'turnover'])
+        df_date_turnover['turnover'] = df_date_turnover['turnover'].fillna(0)
+        return df_date_turnover
 
 
-
-
+prognoliteRestaurantDataExtractor = PrognoliteRestaurantDataExtractor()
