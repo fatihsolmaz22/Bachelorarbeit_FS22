8310949d65dea429b51262eff11e80a124c8b814
Manu Kunnumpurathu <kunnuman@students.zhaw.ch>
2022-04-15 03:31:46 +0200

    implemented turnover_development plot and turnover_per_time_period plot

--- 02_Code/ba_code/ba_code/data_preprocessing/prognolite_restaurant_data_preprocessing/prognolite_restaurant_data_analyzer.py
+++ 02_Code/ba_code/ba_code/data_preprocessing/prognolite_restaurant_data_preprocessing/prognolite_restaurant_data_analyzer.py
@@ -1,5 +1,6 @@
 from prognolite_restaurant_data_extractor import PrognoliteRestaurantDataExtractor
-from ba_code.data_preprocessing.prognolite_restaurant_data_preprocessing.prognolite_restaurant_constants import Restaurant
+from ba_code.data_preprocessing.prognolite_restaurant_data_preprocessing.prognolite_restaurant_constants import \
+    Restaurant
 import matplotlib.pyplot as plt
 
 
@@ -8,26 +9,59 @@ class PrognoliteRestaurantDataAnalyzer:
     def __init__(self):
         self.__restaurantDataExtractor = PrognoliteRestaurantDataExtractor()
 
-    def plot_turnover_per_month_all_restaurants(self):
+    def plot_turnover_per_time_period_for_all_restaurants(self, time_period='m'):
         for restaurant in Restaurant:
-            self.plot_turnover_per_month(restaurant)
+            self.plot_turnover_per_time_period(restaurant, time_period)
 
-    def plot_turnover_per_month(self, restaurant):
+    def plot_turnover_per_time_period(self, restaurant, time_period='m'):
         df_turnover_per_month = self.__restaurantDataExtractor \
-            .get_turnover_per_month_dataframe(restaurant)
+            .get_turnover_per_time_period(restaurant, time_period)
 
         if df_turnover_per_month.empty:
             return
 
         title = "Turnover per month"
-        x_label = "month"
+        x = 'd'
+        y = 'turnover'
+        x_label = self.__get_x_label(time_period)
         y_label = "turnover in CHF"
-        self.__plot_dataframe(df_turnover_per_month,
+        self.__plot_dataframe(df_turnover_per_month, x, y,
                               title, x_label, y_label, restaurant)
 
-    def __plot_dataframe(self, df, title, x_label, y_label, restaurant):
+    def plot_turnover_development_since_beginning_for_all_restaurants(self, time_period='m'):
+        for restaurant in Restaurant:
+            self.plot_turnover_development_since_beginning(restaurant, time_period)
+
+    def plot_turnover_development_since_beginning(self, restaurant, time_period='m'):
+        df_turnover_development_since_beginning = self.__restaurantDataExtractor \
+            .get_turnover_development_since_beginning_dataframe(restaurant, time_period)
+
+        if df_turnover_development_since_beginning.empty:
+            return
+
+        title = "Turnover development since beginning"
+        x = 'd'
+        y = 'turnover'
+        x_label = self.__get_x_label(time_period)
+        y_label = "turnover in CHF"
+        self.__plot_dataframe(df_turnover_development_since_beginning, x, y,
+                              title, x_label, y_label, restaurant)
+
+    def __get_x_label(self, time_period):
+        x_label = ''
+        if time_period == 'd':
+            x_label = 'day'
+        elif time_period == 'm':
+            x_label = 'month'
+        elif time_period == 'Q':
+            x_label = 'quarter year'
+        elif time_period == 'Y':
+            x_label = 'year'
+        return x_label
+
+    def __plot_dataframe(self, df, x, y, title, x_label, y_label, restaurant):
         plt.figure()
-        df.plot(marker='o')
+        df.plot(x=x, y=y, marker='o')
         plt.title(title + ": " + restaurant.value)
         plt.xlabel(x_label)
         plt.ylabel(y_label)
@@ -36,5 +70,4 @@ class PrognoliteRestaurantDataAnalyzer:
         plt.show()
 
 
-restaurantDataAnalyzer = PrognoliteRestaurantDataAnalyzer()
-restaurantDataAnalyzer.plot_turnover_per_month_all_restaurants()
+prognoliteRestaurantDataAnalyzer = PrognoliteRestaurantDataAnalyzer()
