3573631c5c68ab67702b8798bed2879c3da81b7b
Manu Kunnumpurathu <kunnuman@students.zhaw.ch>
2022-04-19 14:39:36 +0200

    implemented overall_rating and turnover development

--- 02_Code/ba_code/ba_code/data_preprocessing/restaurant_review_data_analyzer.py
+++ 02_Code/ba_code/ba_code/data_preprocessing/restaurant_review_data_analyzer.py
@@ -1,41 +1,83 @@
 import matplotlib.pyplot as plt
-from ba_code.data_preprocessing.prognolite_restaurant_data_preprocessing.prognolite_restaurant_data_extractor import PrognoliteRestaurantDataExtractor
-from ba_code.data_preprocessing.tripadvisor_restaurant_data_preprocessing.review_data_extractor import ReviewDataExtractor
-from ba_code.data_preprocessing.prognolite_restaurant_data_preprocessing.prognolite_restaurant_constants import PrognoliteRestaurantDataUri
-from ba_code.data_preprocessing.tripadvisor_restaurant_data_preprocessing.tripadvisor_restaurant_data_uri import TripadvisorRestaurantDataUri
+from ba_code.data_preprocessing.prognolite_restaurant_data_preprocessing.prognolite_restaurant_data_extractor import \
+    PrognoliteRestaurantDataExtractor
+from ba_code.data_preprocessing.tripadvisor_restaurant_data_preprocessing.tripadvisor_restaurant_data_extractor import \
+    TripadvisorRestaurantDataExtractor
+from ba_code.data_preprocessing.prognolite_restaurant_data_preprocessing.prognolite_restaurant_constants import \
+    Restaurant
+from ba_code.data_preprocessing.tripadvisor_restaurant_data_preprocessing.tripadvisor_restaurant_data_uri import \
+    TripadvisorRestaurantDataUri
 
-# TODO: this file is deprecated
-class RestaurantReviewDataAnalyzer:
+
+class PrognoliteTripadvisorRestaurantDataAnalyzer:
 
     def __init__(self):
-        self.__restaurantDataExtractor = PrognoliteRestaurantDataExtractor()
-        self.__reviewDataExtractor = ReviewDataExtractor()
+        self.__prognoliteRestaurantDataExtractor = PrognoliteRestaurantDataExtractor()
+        self.__tripadvisor_restaurant_data_extractors = dict()
+        self.__initialize_tripadvisor_restaurant_data_extractors()
+        self.__restaurants = dict(zip(Restaurant, TripadvisorRestaurantDataUri))
 
-    def plot_restaurant_rating_and_turnover(self, restaurant_uri, review_uri):
-        df_restaurant_data = self.__restaurantDataExtractor.get_turnover_per_month_dataframe(restaurant_uri)
-        df_review_data = self.__reviewDataExtractor.get_overall_monthly_rating_for_restaurant_dataframe(review_uri)
+    def __initialize_tripadvisor_restaurant_data_extractors(self):
+        for tripadvisor_restaurant_data_uri in TripadvisorRestaurantDataUri:
+            tripadvisor_restaurant_data_extractor = TripadvisorRestaurantDataExtractor()
+            tripadvisor_restaurant_data_extractor.load_restaurant_data(open(tripadvisor_restaurant_data_uri.value))
+            self.__tripadvisor_restaurant_data_extractors[tripadvisor_restaurant_data_uri] = \
+                tripadvisor_restaurant_data_extractor
 
-        # plot ###################################################################
+    def plot_development_of_overall_rating_and_turnover_since_beginning_for_all_restaurants(self, time_period='m'):
+        for restaurant in self.__restaurants:
+            self.plot_development_of_overall_rating_and_turnover_since_beginning(restaurant, time_period)
+
+    def plot_development_of_overall_rating_and_turnover_since_beginning(self, restaurant, time_period='m'):
+        df_turnover_development_since_beginning = self.__prognoliteRestaurantDataExtractor \
+            .get_turnover_development_since_beginning_dataframe(restaurant, time_period)
+
+        tripadvisor_restaurant_data_uri = self.__restaurants[restaurant]
+        df_overall_rating_development_since_beginning = \
+            self.__tripadvisor_restaurant_data_extractors[tripadvisor_restaurant_data_uri] \
+                .get_overall_rating_development_since_beginning_dataframe(time_period)
+
+        title = "Turnover and overall rating development over days"
+        x1 = 'd'
+        y1 = 'turnover'
+        x2 = 'date'
+        y2 = 'overall_rating_development'
+
+        # find x_min and x_max limits
+        if time_period != 'Q':
+            x_min_date_turnover = df_turnover_development_since_beginning[x1].to_numpy().min().tz_localize(None)
+            x_max_date_turnover = df_turnover_development_since_beginning[x1].to_numpy().max().tz_localize(None)
+        else:
+            x_min_date_turnover = df_turnover_development_since_beginning[x1].to_numpy().min()
+            x_max_date_turnover = df_turnover_development_since_beginning[x1].to_numpy().max()
+
+        x_min_date_overall_rating = df_overall_rating_development_since_beginning[x2].to_numpy().min()
+        x_min = x_min_date_turnover if x_min_date_turnover < x_min_date_overall_rating else x_min_date_overall_rating
+
+        x_max_date_overall_rating = df_overall_rating_development_since_beginning[x2].to_numpy().max()
+        x_max = x_max_date_turnover if x_max_date_turnover > x_max_date_overall_rating else x_max_date_overall_rating
+
+        # plot turnover development for a restaurant
         color = 'red'
         fig, ax1 = plt.subplots()
         ax1.set_xlabel('date')
-        ax1.set_ylabel('rating', color=color)
+        ax1.set_ylabel('turnover in CHF', color=color)
         ax1.tick_params(axis='y', labelcolor=color)
-        df_review_data.plot(ax=ax1, color=color, marker='o')
+        df_turnover_development_since_beginning.plot(x=x1, y=y1, ax=ax1, color=color, marker='o')
 
+        # plot overall rating development for a restaurant in the same plot
         color = 'blue'
         ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-        ax2.set_ylabel('turnover in CHF', color=color)
+        ax2.set_ylabel('overall rating', color=color)
         ax2.tick_params(axis='y', labelcolor=color)
-        df_restaurant_data.plot(ax=ax2, color=color, marker='o')
+        df_overall_rating_development_since_beginning.plot(x=x2, y=y2, ax=ax2, color=color, marker='o')
 
-        fig.tight_layout()  # otherwise the right y-label is slightly clipped
-        plt.title("Restaurant: " + self.__reviewDataExtractor.get_restaurant_name(review_uri))
+        # fig.tight_layout()  # otherwise the right y-label is slightly clipped
+        plt.title(title + ": " + restaurant.value)
+        plt.xlim([x_min, x_max])
         plt.legend(loc="upper left")
+        plt.grid()
         plt.show()
-        # plot ###################################################################"""
 
 
-restaurantReviewDataAnalyzer = RestaurantReviewDataAnalyzer()
-restaurantReviewDataAnalyzer.plot_restaurant_rating_and_turnover(PrognoliteRestaurantDataUri.NOOCH_BADENERSTRASSE,
-                                                                 TripadvisorRestaurantDataUri.NOOCH_BADENERSTRASSE)
+prognoliteTripadvisorRestaurantDataAnalyzer = PrognoliteTripadvisorRestaurantDataAnalyzer()
