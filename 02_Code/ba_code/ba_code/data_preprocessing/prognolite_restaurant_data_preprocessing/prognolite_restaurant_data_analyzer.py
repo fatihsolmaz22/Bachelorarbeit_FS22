35b6a6c3a008c9dca02745a7839b467424a8935c
Manu Kunnumpurathu <kunnuman@students.zhaw.ch>
2022-04-23 15:12:31 +0200

    added todos and fixed time_period 'Q' and did some renaming

--- 02_Code/ba_code/ba_code/data_preprocessing/prognolite_restaurant_data_preprocessing/prognolite_restaurant_data_analyzer.py
+++ 02_Code/ba_code/ba_code/data_preprocessing/prognolite_restaurant_data_preprocessing/prognolite_restaurant_data_analyzer.py
@@ -20,18 +20,20 @@ class PrognoliteRestaurantDataAnalyzer:
         if df_turnover_per_time_period.empty:
             return
 
-        title = "Turnover per month"
+        title = "Turnover per " + self.__get_value_of_time_period(time_period)
         x = 'd'
         y = 'turnover_per_time_period'
-        x_label = self.__get_x_label(time_period)
+        x_label = self.__get_value_of_time_period(time_period)
         y_label = "turnover in CHF"
         self.__plot_dataframe(df_turnover_per_time_period, x, y,
                               title, x_label, y_label, restaurant)
 
+    # TODO: remove this method later, this is not what Martin asked for
     def plot_turnover_development_since_beginning_for_all_restaurants(self, time_period='m'):
         for restaurant in Restaurant:
             self.plot_turnover_development_since_beginning(restaurant, time_period)
 
+    # TODO: remove this method later, this is not what Martin asked for
     def plot_turnover_development_since_beginning(self, restaurant, time_period='m'):
         df_turnover_development_since_beginning = self.__restaurantDataExtractor \
             .get_turnover_development_since_beginning_dataframe(restaurant, time_period)
@@ -42,27 +44,27 @@ class PrognoliteRestaurantDataAnalyzer:
         title = "Turnover development since beginning"
         x = 'd'
         y = 'turnover'
-        x_label = self.__get_x_label(time_period)
+        x_label = self.__get_value_of_time_period(time_period)
         y_label = "turnover in CHF"
         self.__plot_dataframe(df_turnover_development_since_beginning, x, y,
                               title, x_label, y_label, restaurant)
 
-    def __get_x_label(self, time_period):
-        x_label = ''
+    def __get_value_of_time_period(self, time_period):
+        time_period_value = ''
         if time_period == 'd':
-            x_label = 'day'
+            time_period_value = 'day'
         elif time_period == 'm':
-            x_label = 'month'
+            time_period_value = 'month'
         elif time_period == 'Q':
-            x_label = 'quarter year'
+            time_period_value = 'quarter year'
         elif time_period == 'Y':
-            x_label = 'year'
-        return x_label
+            time_period_value = 'year'
+        return time_period_value
 
     def __plot_dataframe(self, df, x, y, title, x_label, y_label, restaurant):
         plt.figure()
         df.plot(x=x, y=y, marker='o')
-        plt.title(title + ": " + restaurant.value)
+        plt.title(title + ":\n" + restaurant.value)
         plt.xlabel(x_label)
         plt.ylabel(y_label)
         plt.legend(loc="upper left")
@@ -71,5 +73,4 @@ class PrognoliteRestaurantDataAnalyzer:
 
 
 prognoliteRestaurantDataAnalyzer = PrognoliteRestaurantDataAnalyzer()
-# prognoliteRestaurantDataAnalyzer.plot_turnover_development_since_beginning(Restaurant.BUTCHER_USTER, 'Q')
 # prognoliteRestaurantDataAnalyzer.plot_turnover_per_time_period(Restaurant.BUTCHER_USTER, 'Q')
--- 02_Code/ba_code/ba_code/data_preprocessing/prognolite_restaurant_data_preprocessing/prognolite_restaurant_data_extractor.py
+++ 02_Code/ba_code/ba_code/data_preprocessing/prognolite_restaurant_data_preprocessing/prognolite_restaurant_data_extractor.py
@@ -27,6 +27,7 @@ class PrognoliteRestaurantDataExtractor:
     def get_restaurant_data_dataframe(self, restaurant):
         return self.__restaurant_data[restaurant.value]
 
+    # TODO: remove this method later, this is not what Martin asked for
     def get_turnover_development_since_beginning_dataframe(self, restaurant, time_period='m'):
         df_date_turnover = self.__get_date_turnover_dataframe(restaurant)
 
@@ -49,14 +50,10 @@ class PrognoliteRestaurantDataExtractor:
         df_date_turnover = self.__get_date_turnover_dataframe(restaurant)
 
         df_date_turnover_per_time_period = None
-        if time_period == 'd' or time_period == 'm' or time_period == 'Y':
+        if time_period == 'd' or time_period == 'm' or time_period == 'Q' or time_period == 'Y':
             df_date_turnover_per_time_period = \
                 df_date_turnover.groupby(pd.Grouper(key='d', axis=0, freq=time_period)).sum() \
                     .rename(columns={"turnover": "turnover_per_time_period"}).reset_index()
-        elif time_period == 'Q':
-            df_date_turnover_per_time_period = \
-                df_date_turnover.groupby(df_date_turnover['d'].dt.to_period(time_period))['turnover'].agg(
-                    'sum').reset_index().rename(columns={"turnover": "turnover_per_time_period"})
         else:
             self.__print_invalid_time_period_message()
             return
